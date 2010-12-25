16a7e72e9ec37d54661e853e750ffe1023eea58c
Okspen <rip4eg@gmail.com>
2010-12-25 18:18:49 +0200

    Удален инклуд класса QGraphicsScene. Теперь он инклудится в родительском классе AGFGraphicsItem.

--- items/agfgrid.cpp
+++ items/agfgrid.cpp
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "agfgrid.h"
-#include <QGraphicsScene>
 
 AGFGrid::AGFGrid(QGraphicsItem* parent): AGFGraphicsItem(parent) {
     setQuarterVisible(1, true);
